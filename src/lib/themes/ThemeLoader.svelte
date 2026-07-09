7f8052dc1daeffb07b657982f68131fa831a5b0e
DonnerTechQ2 <quick.take1@outlook.com>
2026-07-09 20:16:40 +0000

    Simplify ThemeLoader

--- src/lib/themes/ThemeLoader.svelte
+++ src/lib/themes/ThemeLoader.svelte
@@ -1,37 +1,11 @@
 <script lang="ts">
 	import { themeState } from './index.svelte.js';
-	// Dynamic import of the component based on theme name and component name
-	
 	let { componentName, ...props } = $props<{ componentName: string, [key: string]: any }>();
-	
-	let LoadedComponent = $state<any>(null);
-	
-	$effect(() => {
-		let isMounted = true;
-		
-		const loaders = import.meta.glob('./*/*.svelte');
-		
-		const load = async () => {
-			const path = `./${themeState.value}/${componentName}.svelte`;
-			if (loaders[path]) {
-				const module = await loaders[path]();
-				if (isMounted) {
-					LoadedComponent = (module as any).default;
-				}
-			} else {
-				console.error(`Component ${componentName} for theme ${themeState.value} not found`);
-				LoadedComponent = null;
-			}
-		};
-		
-		load();
-		
-		return () => {
-			isMounted = false;
-		};
-	});
 </script>
 
-{#if LoadedComponent}
+{#await import(`./${themeState.value}/${componentName}.svelte`) then module}
+	{@const LoadedComponent = module.default}
 	<LoadedComponent {...props} />
-{/if}
+{:catch}
+	<!-- fallback if not found -->
+{/await}
