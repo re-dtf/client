057b232ce064b12ffaa666cc14d30a01e2fb8f43
DonnerTechQ2 <quick.take1@outlook.com>
2026-07-10 04:06:54 +0000

    Simplify lazyvideo code

--- src/lib/actions/lazyVideo.ts
+++ src/lib/actions/lazyVideo.ts
@@ -1,19 +1,6 @@
 export function lazyVideo(node: HTMLVideoElement) {
 	const observer = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				// Play if visible
-				const playPromise = node.play();
-				if (playPromise !== undefined) {
-					playPromise.catch(() => {
-						// Autoplay prevented by browser, safe to ignore
-					});
-				}
-			} else {
-				// Pause if hidden to save CPU
-				node.pause();
-			}
-		});
+		entries[0].isIntersecting ? node.play()?.catch(() => {}) : node.pause();
 	}, { rootMargin: '200px' });
 
 	observer.observe(node);
